03350033592d68d6796ad3dd3262cd5122f4c7df
Arunkumar Parthasarathy <arunkumar.ps1988@gmail.com>
2023-03-22 17:10:23 +0530

    Local function initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Local function example");
+        int a = 40, b = 20;
+        int sum = Sum(a, b);
+        int difference = Difference(a, b);
+
+        Console.WriteLine($"The Sum of {a} and {b} is {sum}");
+        Console.WriteLine($"The Difference of {a} and {b} is {difference}");
+
+        int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        int Difference(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
